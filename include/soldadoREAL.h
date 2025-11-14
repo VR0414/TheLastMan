be33813c74dce375a9da0a6ccf88e6b75b23018c
Diego Magnata <dfm@cesar.school>
2025-11-14 08:55:30 -0300

    Adiciona cabeçalho da função de resetar posição após tomar dano das balas

--- include/soldadoREAL.h
+++ include/soldadoREAL.h
@@ -20,7 +20,6 @@ void iniciar_soldado(Soldado *s, float linha, float coluna, int vida, char simbo
 
 int mover_soldado(Soldado *s, Mapa *mapa, float deslocLinha, float deslocColuna);
 
-
-
+int reset_posicao_soldado(Soldado *s, float linha, float coluna);
 
 #endif
