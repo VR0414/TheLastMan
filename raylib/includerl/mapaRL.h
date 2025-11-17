96d89bf32b2137bf2e24b8cbc2bee4ce0a6f68d1
Matheus Fialho <mgfm@cesar.school>
2025-11-16 22:26:32 -0300

    Refactor Mapa struct and update function signatures

--- raylib/includerl/mapaRL.h
+++ raylib/includerl/mapaRL.h
@@ -4,19 +4,32 @@
 #define MAPA_LINHAS 24
 #define MAPA_COLUNAS 80
 
+// Caracteres padrão
+#define CEL_PAREDE '#'
+#define CEL_CHAO   '.'
+#define CEL_ABRIGO '='
+
 typedef struct Mapa {
     int linhas;
     int colunas;
-    char abrigo;
-    char *celulas;
+    char abrigo;      // símbolo do abrigo
+    char parede;      // símbolo da parede
+    char vazio;       // símbolo do chão/solo
+    char *celulas;    // grade 2D armazenada em vetor linear
 } Mapa;
 
-Mapa *criar_mapa(int linhas, int colunas, char abrigo);
+Mapa *criar_mapa(int linhas, int colunas, char abrigo, char parede, char vazio);
 void liberar_mapa(Mapa *mapa);
-int preencher_limites(Mapa *mapa, char parede);
-void preencher_chao(Mapa *mapa, char vazio, char parede, char abrigo);
-void preencher_abrigo(Mapa *mapa, char abrigo);
+
+// Construção
+int preencher_limites(Mapa *mapa);
+void preencher_chao(Mapa *mapa);
+void preencher_abrigo(Mapa *mapa);
+
+// Utilidades
 void imprimir_mapa(Mapa *mapa);
-void desenhar_mapa(Mapa *mapa, int tamanho_celula);  // Raylib
+
+// Raylib
+void desenhar_mapa(Mapa *mapa, int tamanho_celula);
 
 #endif
