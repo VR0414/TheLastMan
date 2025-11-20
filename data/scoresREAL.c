8e963144f9b19bfbc9b713d336c74a72bf279c52
Diego Magnata <dfm@cesar.school>
2025-11-20 09:00:31 -0300

    feature: Implementa função de impressão de dados (score)

--- data/scoresREAL.c
+++ data/scoresREAL.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "scoresREAL.h"
 
-void imprimir_score(Score *lista) {
+void imprimir_score(Score *head) {
+    Score *atual = head;
+    int posicao = 1;
 
-    
+    while (atual) {
+        printf("%d. %.2f segundos\n", posicao, atual->tempo);
+        atual = atual->prox;
+        posicao++;
+    }
 }
