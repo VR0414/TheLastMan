edb9fbff70475b081adc0ae2aeb983784ef5e9ee
Diego Magnata <dfm@cesar.school>
2025-11-20 09:03:45 -0300

    feature: Implementa função de escrita no arquivo de dados

--- data/scoresREAL.c
+++ data/scoresREAL.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include "scoresREAL.h"
 
+void salvar_scores(char *arquivo, Score *head) {
+    FILE *f = fopen(arquivo, "w");
+    if (!f) return;
+
+    Score *atual = head;
+
+    int contador = 0;
+    while (atual && contador < 3) {   // salva só os 3 melhores
+        fprintf(f, "%.2f\n", atual->tempo);
+        atual = atual->prox; 
+        contador++;
+    }
+
+    fclose(f);
+}
+
 void imprimir_score(Score *head) {
     Score *atual = head;
     int posicao = 1;
